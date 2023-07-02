719ec2176e73e2896d0bff91bf0de35fb8133492
lovebox0310 <57202722+lovebox0310@users.noreply.github.com>
2023-07-02 18:55:36 +0900

    add start-pause button.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM Content Loaded...')
-})
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM Content Loaded...");
+  const grid = document.querySelector(".grid");
+  let square = document.querySelectorAll(".grid div");
+
+});
